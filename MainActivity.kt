f4062cceae25094061316accb4a9086ea9d4e52c
Riust <hardskill808@gmail.com>
2025-10-16 11:16:35 +0700

    Create MainActivity.kt

---
+++ MainActivity.kt
@@ -0,0 +1,16 @@
+package com.example.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.ImageView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        // Sesuai dengan ID di XML
+        val imageView = findViewById<ImageView>(R.id.imageView)
+        imageView.setImageResource(R.drawable.op)
+    }
+}
